lwc/wsgid

commit 4390d43535b14ad34053ad8b1d39dcc0537e2b8a

Simple loading of extra django options
Simple options (string only) are being loaded and
already overrides any original setting

ref #18

wsgid/loaders/djangoloader.py
@@ -3,9 +3,11 @@
 
 from wsgid.loaders import IAppLoader
 from wsgid.core import Plugin, get_main_logger
+from django.conf import settings
 
 import os
 import sys
+import simplejson
 
 log = get_main_logger()
 
@@ -31,17 +33,34 @@ class DjangoAppLoader(Plugin):
   def can_load(self, app_path):
     return self._first_djangoproject_dir(app_path) is not None
 
+  def _load_django_extra_options(self, path):
+      parsed = {}
+      conf_file = os.path.join(path, 'django.json')
+      log.debug("Reading {0}".format(conf_file))
+      if os.path.exists(conf_file):
+        parsed = simplejson.load(open(conf_file))
+      return parsed
 
-  def load_app(self, app_path, app_full_name):
+  def load_app(self, app_path, app_full_name = None):
     logger = get_main_logger()
+    # Since we receive here --app-path + app/, we need to remove the last part
+    # because django.json lives in --app-path
+    wsgidapp_path = os.path.dirname(app_path)
+
+    extra = self._load_django_extra_options(wsgidapp_path)
 
     site_name = self._first_djangoproject_dir(app_path)
     os.environ['DJANGO_SETTINGS_MODULE'] = '{0}.settings'.format(site_name)
     logger.debug("Using DJANGO_SETTINGS_MODULE = {0}".format(os.environ['DJANGO_SETTINGS_MODULE']))
 
-    new_sys_path = os.path.join(app_path, site_name)
-    logger.debug("Adding {0} to sys.path".format(new_sys_path))
-    sys.path.insert(0, new_sys_path)
+    logger.debug("Adding {0} to sys.path".format(app_path))
+    sys.path.insert(0, app_path)
+
+    # Here we force django to load the app settings
+    settings._some_value = True
+    del settings._some_value
+    for k,v in extra.items():
+        setattr(settings, k, v)
 
     import django.core.handlers.wsgi
     return django.core.handlers.wsgi.WSGIHandler()

tests/unit/djangoloader.py
@@ -106,9 +106,15 @@ class DjangoLoaderTest(unittest.TestCase):
    after DjangoAppLodare loads this app.
   '''
   def test_override_existing_settings_option(self):
+      app_path = os.path.join(FIXTURE, WSGID_APP_NAME, 'app')
+      self.app_loader.load_app(app_path)
+      self.assertEquals('other-value', settings.MY_CUSTOM_SETTING)
+
+  '''
+   We must log any parse error that we may find when reading django.json
+  '''
+  def test_log_error_when_parsing_django_json(self):
       self.fail()
-
-
 
 
 

tests/unit/django-wsgid-app/app/mydjangoapp/settings.py
@@ -9,6 +9,8 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+MY_CUSTOM_SETTING = "Custom"
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.

commit 550b7900a511ee93b3a16a582f650cfe3ac927a7

Django project folder must be added to sys.path
This reverts a bug caused by commit 4390d43.

ref #32

wsgid/loaders/djangoloader.py
@@ -56,8 +56,12 @@ class DjangoAppLoader(Plugin):
     os.environ['DJANGO_SETTINGS_MODULE'] = '{0}.settings'.format(site_name)
     logger.debug("Using DJANGO_SETTINGS_MODULE = {0}".format(os.environ['DJANGO_SETTINGS_MODULE']))
 
+    new_syspath = os.path.join(app_path, site_name)
+
     logger.debug("Adding {0} to sys.path".format(app_path))
-    sys.path.insert(0, app_path)
+    sys.path.append(app_path)
+    logger.debug("Adding {0} to sys.path".format(new_syspath))
+    sys.path.append(new_syspath)
 
     # Here we force django to load the app settings
     settings._some_value = True

tests/unit/djangoloader.py
@@ -54,6 +54,21 @@ class DjangoLoaderTest(unittest.TestCase):
       djangoapp_path = os.path.join(FIXTURE, 'wsgidapp-noinit/app')
       self.assertFalse(self.app_loader.can_load(djangoapp_path))
 
+  def test_django_project_folder_added_to_sys_path(self):
+      with patch('sys.path') as mock_syspath:
+        with patch('os.listdir'):
+            dirs = ['mydjangoapp', 'otherdjangoapp']
+            os.listdir.return_value = dirs
+            self.assertTrue(self.app_loader.can_load(self.wsgid_appfolder_fullpath))
+
+            os.listdir.return_value = list(reversed(dirs))
+            self.app_loader.load_app(self.wsgid_appfolder_fullpath, 'appname')
+            self.assertEquals("mydjangoapp.settings", os.environ['DJANGO_SETTINGS_MODULE'])
+
+            djangoproject_folder = os.path.join(self.wsgid_appfolder_fullpath, 'mydjangoapp')
+            expected = [((self.wsgid_appfolder_fullpath,), {}), ((djangoproject_folder,), {})]
+            self.assertEquals(expected ,mock_syspath.append.call_args_list)
+
   '''
    Check if we return False for a non-django app folder
   '''